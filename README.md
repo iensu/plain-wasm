36172b0df8d5746215907f395beeb6323df3acb2
Jens Östlund <jostlund@gmail.com>
2020-10-11 12:09:53 +0200

    Add browser version

--- README.md
+++ README.md
@@ -7,7 +7,12 @@ Playing around with plain WebAssembly.
 * NodeJS
 * WebAssembly Build Tools (WABT)
 
-## Run the code
+## Run the code in node.js
 
 1. `wat2wasm src/upper.wat -o src/upper.wasm`
 2. node src/index.js
+
+## Run the code in a browser
+
+1. Start a server in the current directory, for instance: `python3 -m http.server`
+2. Navigate to `http://localhost:<PORT>/src/index.html`
--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 const fs = require("fs");
 const path = require("path");
 
+/**
+ * Writes a string to the provided memory buffer and returns the number of written bytes.
+ */
 function setString(memory, s) {
-  const u8array = new Uint8Array(memory.buffer);
+  const u8array = new Uint8Array(memory.buffer); // View the memory buffer as an array of bytes
   const encoded = new TextEncoder().encode(s);
-  u8array.set(encoded);
+  u8array.set(encoded); // Write encoded string to the memory buffer as bytes
 
   return encoded.length;
 }
 
-function getString(memory) {
-  const u8array = new Uint8Array(memory.buffer);
-  return new TextDecoder().decode(u8array);
+/**
+ * Returns the content of the memory buffer as a string
+ */
+function getString(memory, length) {
+  const u8array = new Uint8Array(memory.buffer); // View memory buffer as an array of bytes
+  const decoded = new TextDecoder().decode(
+    u8array.slice(0, length || u8array.length)
+  );
+
+  return decoded;
 }
 
+/**
+ * Loads a WebAssembly module and returns it.
+ */
+async function loadWasm(filePath, moduleConfig) {
+  const bytes = fs.readFileSync(path.join(__dirname, filePath));
+
+  return WebAssembly.instantiate(bytes, moduleConfig);
+}
+
+/* SCRIPT START */
+
 (async function run() {
-  const bytes = fs.readFileSync(path.join(__dirname, "./upper.wasm"));
   const memory = new WebAssembly.Memory({ initial: 1, maximum: 10 });
-  const { instance } = await WebAssembly.instantiate(bytes, {
+
+  const { instance } = await loadWasm("./upper.wasm", {
     env: { memory }
   });
 
@@ -28,7 +49,7 @@ function getString(memory) {
 
   instance.exports.toUpper(0, byteLength);
 
-  const result = getString(memory);
+  const result = getString(memory, byteLength);
 
   console.log(result);
 })();
